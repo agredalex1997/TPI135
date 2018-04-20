759a1aa6ea64adae4e35edf4ce0645f1dfa000ff
ikkiwar <rodriguezkev07@gmail.com>
2018-04-20 00:20:11 -0600

    se aplico el AbstractInfterface

---
+++ SistemaTPI135-ejb/src/main/java/ues/edu/sv/mantenimientoTPI/acceso/ExcepcionCalendarioFacadeLocal.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ues.edu.sv.mantenimientoTPI.acceso;
+
+import java.util.List;
+import javax.ejb.Local;
+import ues.edu.sv.tpi135.entidades.ExcepcionCalendario;
+
+/**
+ *
+ * @author kevin
+ */
+@Local
+public interface ExcepcionCalendarioFacadeLocal extends AbstractFacadeInterface<ExcepcionCalendario>{
+
+    
+}
