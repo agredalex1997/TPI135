4dfcb22427711d4740f1ff3fcc27b4bbae2a03fb
esperanza <da13002@ues.edu.sv>
2018-04-27 20:58:53 -0600

    pruebas para ejb

---
+++ SistemaTPI135-ejb/src/test/java/ues/edu/sv/mantenimientoTPI/acceso/EntityManagerProvider.java
@@ -0,0 +1,69 @@
+
+package ues.edu.sv.mantenimientoTPI.acceso;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.rules.TestRule;
+import org.junit.runner.Description;
+import org.junit.runners.model.Statement;
+
+/**
+ *
+ * @author esperanza
+ */
+public class EntityManagerProvider implements TestRule{
+   
+    protected static EntityManagerFactory emf;
+    protected static EntityManager em;
+    protected static EntityTransaction tx;
+
+ 
+
+    @After
+    public void cleanUp() {
+        em.getTransaction().rollback();
+    }
+
+    @AfterClass
+    public static void tearDown() {
+        em.clear();
+        em.close();
+        emf.close();
+    }
+
+    EntityManagerProvider(String unitName) {
+        this.em = Persistence.createEntityManagerFactory(unitName).createEntityManager();
+        this.tx = this.em.getTransaction();
+    }
+
+    public final static EntityManagerProvider persistenceUnit(String unitName) {
+        return new EntityManagerProvider(unitName);
+    }
+
+    public EntityManager em() {
+        return this.em;
+    }
+
+    public EntityTransaction tx() {
+        return this.tx;
+    }
+
+    @Override
+    public Statement apply(final Statement base, Description description) {
+        return new Statement() {
+
+            @Override
+            public void evaluate() throws Throwable {
+                base.evaluate();
+                em.clear();
+            }
+
+        };
+    }
+
+}
